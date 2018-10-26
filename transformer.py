2d4af0475575ce14149e9f627c01082904b93d24
Yang Dai <yangdai713@aim.com>
2018-10-26 17:38:47 -0400

    analysis

--- transformer.py
+++ transformer.py
@@ -1,7 +1,10 @@
+import numpy as np
+
+
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
-class DataFrameConverter(BaseEstimator, TransformerMixin):
+class DataFrameSelecter(BaseEstimator, TransformerMixin):
     def __init__(self, attribute_names):
         self.attribute_names = attribute_names
 
@@ -9,33 +12,64 @@ class DataFrameConverter(BaseEstimator, TransformerMixin):
         return self     # nothing to do here
 
     def transform(self, X):
-        return X[self.attribute_names].values
+        return X[self.attribute_names]
 
 
 class DiscountConverter(BaseEstimator, TransformerMixin):
-    def __init__(self, attribute_name='Discount_rate'):
-        self.attribute_name = attribute_name
-
     def fit(self, X, y=None):
         return self
 
     def transform(self, X, y=None):
+        X = X.fillna(0.)
 
         def convert_discount(x):
-            x = str(x)
-            if ':' in x:
-                x1 = x.split(':')[0]
-                x2 = x.split(':')[1]
-                rate = (float(x1) - float(x2)) / float(x1)
-
-            else:
-                try:
-                    rate = float(x)
-                except ValueError:
+            try:
+                if ':' in x:
+                    x1 = x.split(':')[0]
+                    x2 = x.split(':')[1]
+                    rate = (float(x1) - float(x2)) / float(x1)
+                else:
                     rate = x
+
+            except TypeError:
+                rate = 0
 
             return rate
 
-        X[self.attribute_name] = X[self.attribute_name].apply(convert_discount)
+        X = X.applymap(convert_discount)
+
+        return X
+
+
+class DateToWeekConverter(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+
+        def convert_date(x):
+            try:
+                x = x.dt.weekday
+            except AttributeError:
+                pass
+
+            return x
+
+        X = X.apply(convert_date)
+        X.fillna(7, inplace=True)
+
+        return X
+
+
+class CategoryConverter(BaseEstimator, TransformerMixin):
+    def __init__(self, scale=1):
+        self.scale = scale
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = np.ceil(X / self.scale)
+        X.where(X > 1, 1, inplace=True)
 
         return X
